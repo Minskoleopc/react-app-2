68ae68b52c5044ddc9b52b63ce1e463c7da2757f
unknown <Minskoleopc>
2024-03-03 20:11:20 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
+import LifecyleA from './components/LifecycleA';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+      {/* <Form/> */}
+      <LifecyleA/>
     </div>
   );
 }
